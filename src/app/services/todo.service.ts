3bbb9bf2d328c5e34eaeb519dcf6262245b33576
mohammed <mohammedalmahfoodh@yahoo.com>
2018-11-30 18:45:35 +0100

    add localStorage functionality

--- src/app/services/todo.service.ts
+++ src/app/services/todo.service.ts
@@ -4,31 +4,40 @@ import { Todo } from '../classes/todo';
 @Injectable()
 export class TodoService {
 
-  private todos: Todo[];
+  
   private nextId: number;
 
   constructor() {
-    this.todos = [
-      new Todo(0, "Make dinner tonight!"),
-      new Todo(1, "Fold the laundry."),
-      new Todo(2, "Learn to make a React app!")
-    ];
+   let todos=this.getTodos();
 
-    this.nextId = 3;
+    if(todos.length==0){
+      this.nextId=0;
+    }else{
+      let maxId=todos[todos.length-1].id;
+      this.nextId=maxId+1;
+    }
   }
 
   public addTodo(text: string): void {
     let todo = new Todo(this.nextId, text);
-    this.todos.push(todo);
+   let todos=this.getTodos()
+   todos.push(todo);
+   this.setLocalStorageTodos(todos);
     this.nextId++;
   }
 
   public getTodos(): Todo[] {
-    return this.todos;
+    let localStorageItem=JSON.parse(localStorage.getItem('todos'));
+    return localStorageItem==null?[]:localStorageItem.todos;
   }
 
   public removeTodo(id: number): void {
-    this.todos = this.todos.filter((todo)=> todo.id != id);
+    let todos=this.getTodos();
+    todos = todos.filter((todo)=> todo.id != id);
+    this.setLocalStorageTodos(todos);
   }
+  private setLocalStorageTodos(todos:Todo[]):void{
+    localStorage.setItem('todos',JSON.stringify({todos:todos}));
+   }
 
 }
